d0224a7da4fca6ef84a0d838c390b1874ea0dd78
Josué Munganga <jmunganga039@gmail.com>
2021-12-07 11:41:33 +0200

    Working on the User model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    email: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+},
+{ timestamps: true }
+)
+
+module.exports = User = mongoose.model('user', UserSchema);
