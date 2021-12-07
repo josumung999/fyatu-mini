481667c7042390e95869aaa3e1370a33201f66ab
Josué Munganga <jmunganga039@gmail.com>
2021-12-07 12:42:33 +0200

    JWT Implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 const { check, validationResult } = require('express-validator');
 
 // importing the User Model
@@ -46,8 +48,23 @@ router.post('/', [
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
+
     // Return jsonwebtoken
-    res.send('User registered');
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if(err)  throw err;
+        res.json({ token })
+      }
+    );
   } catch(err) {
     console.error(err.message);
     res.status(500).send('Server error')
