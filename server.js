61c09b7b04d4445eb0133339fec91a6a22bb7dd4
Josué Munganga <jmunganga039@gmail.com>
2021-12-07 12:00:13 +0200

    Setting up express validator for user routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
